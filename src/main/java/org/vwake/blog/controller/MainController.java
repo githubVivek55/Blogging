32253fe85025240da65d600af7c6242f33a966bc
githubVivek55 <vivekpce55@outlook.com>
2017-05-28 15:22:24 +0530

    New Project Structure created-vwake

---
+++ src/main/java/org/vwake/blog/controller/MainController.java
@@ -0,0 +1,12 @@
+package org.vwake.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+	public String mappingToindex(){
+		return "index";
+	}
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+ 
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h2>Welcome</h2>
+        </div>
+    </div>
+</body>
+</html
